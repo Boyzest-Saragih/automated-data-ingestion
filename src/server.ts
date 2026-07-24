6d4a7d51df19b75178afa69bd916b48194717ab8
boyzest <boyzestsumbayak@gmail.com>
2026-07-24 21:40:05 +0700

    feat: update server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
-import app from './app.js';
-import dotenv from 'dotenv';
+import app from "./app.js";
+import dotenv from "dotenv";
+import { IngestionScheduler } from "./schedulers/ingestion.scheduler.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Ingestion API running on port http://localhost:${PORT}`);
+  console.log(`Ingestion API running on port http://localhost:${PORT}`);
+  const scheduler = new IngestionScheduler();
+  scheduler.init();
 });
